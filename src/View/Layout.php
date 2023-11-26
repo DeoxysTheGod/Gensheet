afe0a7fd1ffd08f7079c3614985fe9136f619764
DeoxysTheGod <quentinb2004@gmail.com>
2023-11-26 19:49:43 +0100

    Node and npm + adds

--- src/View/Layout.php
+++ src/View/Layout.php
@@ -14,8 +14,7 @@ class Layout
                 <link rel="stylesheet" href="/assets/styles/css/<?= $style ?>.css">
             <?php endforeach; ?>
             <script src="/assets/scripts/js/main.js" defer></script>
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2694644766774650"
-     crossorigin="anonymous"></script>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2694644766774650" crossorigin="anonymous"></script>
             <link rel="icon" href="/favicon.ico" type="image/x-icon">
 			<title><?= $title ?></title>
 		</head>
